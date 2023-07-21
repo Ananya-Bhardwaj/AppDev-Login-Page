0eb9bc72f1d4faa6f4cd510be80a8e26f38c8a85
Ananya <ananya.bhar@gmail.com>
2023-07-21 22:00:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(80.0),
+            child: FilledButton(
+              onPressed: () {},
+              child: const Text('Skip'),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color?>(Colors.amber[200]),
+                alignment: const Alignment(1, 1),
+              ),
+            ),
+          ),
+          body: Center(
+            child: Column(children: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('Continue with Email'),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.green[600],
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Icon(Icons.apple),
+                  const Text('Continue with Apple'),
+                ],
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.red,
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                ),
+              child: Container(
+                width: 100,
+                child: Row(
+                  children: [
+                    Icon(Icons.g_translate_rounded),
+                    const Text('Continue with Google'),
+                  ],
+                ),
+              ),
+            ),
+              TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Icon(Icons.facebook),
+                    const Text('Continue with Facebook'),
+                  ],
+                ),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            ],
+        )
+          ),
+          )
+      )
+    );
+}
