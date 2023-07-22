3d64bd1b0a003d28e41b4f253a29d75c8e9e4e90
Ananya <ananya.bhar@gmail.com>
2023-07-22 19:51:05 +0530

    Added email page

--- lib/Email.dart
+++ lib/Email.dart
@@ -1,14 +1,13 @@
-
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class EmailPage extends StatefulWidget {
+  const EmailPage({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<EmailPage> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyWidgetState extends State<EmailPage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
